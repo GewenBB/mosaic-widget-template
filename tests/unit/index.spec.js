ebd5514f6d24a85c5f169cdffbfc17f8b87459d4
dragosh-bb <dragos@backbase.com>
2015-09-11 11:15:18 +0200

    fix test path

--- tests/unit/index.spec.js
+++ tests/unit/index.spec.js
@@ -1,4 +1,4 @@
-import ${component.transforms.name.camelCase} from './index';
+import ${component.transforms.name.camelCase} from '../../index';
 describe('${component.transforms.name.camelCase}', () => {
    it('should be a function', () => {
        expect(${component.transforms.name.camelCase}).toBeFunction();
