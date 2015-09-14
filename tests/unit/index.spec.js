8bbcdff89428f3c8b9d84f8205027ac1170c7516
dragosh-bb <dragos@backbase.com>
2015-09-14 11:25:54 +0200

    use 4 psace indentation

--- tests/unit/index.spec.js
+++ tests/unit/index.spec.js
@@ -1,6 +1,6 @@
 import ${component.transforms.name.camelCase} from '../../index'
 describe('${component.name}', () => {
-   it('should be a function', () => {
-       expect(${component.transforms.name.camelCase}).toBeFunction()
-   })
+    it('should be a function', () => {
+        expect(${component.transforms.name.camelCase}).toBeFunction()
+    })
 })
